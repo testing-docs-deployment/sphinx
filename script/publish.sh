2354d4ded13d1ade93ecbb0e5597e0ccc5f53f98
Karine Pires <karine.pires@alterway.fr>
2017-01-17 17:21:31 +0100

    Testing

--- script/publish.sh
+++ script/publish.sh
@@ -10,3 +10,10 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out 
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
+
+
+# Push build to repository
+BUILDDIR="build"
+GIT_PUB_LOCAL_DIR="docs_repo"
+ssh-add -l
+git clone $GIT_PUB_REPO $GIT_PUB_LOCAL_DIR && cp -r ${BUILDDIR}/html docs_repo/${GIT_PUB_DIR} && cd $GIT_PUB_LOCAL_DIR && git add . && git commit -m "Updating docs." && git push $GIT_PUB_REPO $GIT_PUB_BRANCH
